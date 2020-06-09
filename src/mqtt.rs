1d74619484c17ffb22f9ac4b6e84580f0dc64f1b
sweetim <hosweetim@gmail.com>
2020-06-09 23:31:50 +0900

    added mqtt msg parsing

--- src/mqtt.rs
+++ src/mqtt.rs
@@ -1,4 +1,5 @@
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize, Serialize, Deserializer};
+use super::control::*;
 
 #[derive(Deserialize, Serialize, Clone, Debug)]
 pub struct RequestPayload {
@@ -82,13 +83,92 @@ impl EnvironmentCurrentSensorData {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct CurrentStateRaw {
+    pub msg: String,
+    pub time: String,
+    #[serde(rename = "mode-reason")]
+    pub mode_reason: String,
+    #[serde(rename = "state-reason")]
+    pub state_reason: String,
+    pub dial: String,
+    #[serde(deserialize_with = "from_string")]
+    pub rssi: i32,
+    #[serde(rename = "product-state")]
+    pub product_state: ProductState,
+    pub scheduler: SchedulerRaw,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ProductState {
+    #[serde(rename = "fmod")]
+    pub fan_mode: FanMode,
+    #[serde(rename = "fnst")]
+    pub fan_state: FanState,
+    #[serde(rename = "fnsp")]
+    pub fan_speed: FanSpeed,
+    #[serde(rename = "qtar")]
+    pub quality_target: QualityTarget,
+    #[serde(rename = "oson")]
+    pub oscillation_status: OscillationStatus,
+    #[serde(rename = "rhtm")]
+    pub air_quality_monitoring_status: AirQualityMonitoringStatus,
+    #[serde(rename = "filf")]
+    #[serde(deserialize_with = "from_string")]
+    pub filter_life: i32,
+    #[serde(rename = "ercd")]
+    pub ercd: String,
+    #[serde(rename = "nmod")]
+    pub night_mode: NightMode,
+    #[serde(rename = "wacd")]
+    pub wacd: String,
+    #[serde(rename = "hmod")]
+    pub heat_mode: HeatMode,
+    #[serde(rename = "hmax")]
+    #[serde(deserialize_with = "from_raw_string_to_kelvin")]
+    pub heat_target_kelvin: f32,
+    #[serde(rename = "hsta")]
+    pub heat_state: HeatState,
+    #[serde(rename = "ffoc")]
+    pub fan_focus_mode: FanFocusMode,
+    #[serde(rename = "tilt")]
+    pub tilt_state: TiltState,
+}
+
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct SchedulerRaw {
+    pub srsc: String,
+    pub dstv: String,
+    pub tzid: String,
+}
+
+fn from_string<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where T: std::str::FromStr,
+          T::Err: std::fmt::Display,
+          D: Deserializer<'de>
+{
+    let s = String::deserialize(deserializer)?;
+    T::from_str(&s).map_err(serde::de::Error::custom)
+}
+
+fn from_raw_string_to_kelvin<'de, T, D>(deserializer: D) -> Result<T, D::Error>
+    where T: std::str::FromStr + std::ops::Div + num_traits::Float + num_traits::FromPrimitive,
+          T::Err: std::fmt::Display,
+          D: Deserializer<'de>
+{
+    let s = String::deserialize(deserializer)?;
+    T::from_str(&s)
+        .map_err(serde::de::Error::custom)
+        .map(|x| x / T::from_f32(10.0).unwrap())
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
     fn it_converts_environment_current_sensor_data_from_raw() {
-        let data = r#"
+        let raw = r#"
             {
                 "tact":"2990",
                 "hact":"0073",
@@ -97,7 +177,7 @@ mod test {
                 "sltm":"OFF"
             }"#;
 
-        let environment_current_sensor_data_raw: EnvironmentCurrentSensorDataRaw = serde_json::from_str(data).unwrap();
+        let environment_current_sensor_data_raw: EnvironmentCurrentSensorDataRaw = serde_json::from_str(raw).unwrap();
 
         let expected = EnvironmentCurrentSensorData{
             humidity_percentage: 73.0,
@@ -108,6 +188,76 @@ mod test {
         };
 
         let actual = EnvironmentCurrentSensorData::from_raw(&environment_current_sensor_data_raw);
+
+        assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn it_converts_current_state_from_raw() {
+        let raw = r#"
+            {
+                "msg": "CURRENT-STATE",
+                "time": "2020-05-19T14:53:04.000Z",
+                "mode-reason": "LSCH",
+                "state-reason": "ENV",
+                "dial": "OFF",
+                "rssi": "-35",
+                "product-state": {
+                    "fmod": "AUTO",
+                    "fnst": "FAN",
+                    "fnsp": "AUTO",
+                    "qtar": "0001",
+                    "oson": "ON",
+                    "rhtm": "ON",
+                    "filf": "3732",
+                    "ercd": "NONE",
+                    "nmod": "ON",
+                    "wacd": "NONE",
+                    "hmod": "OFF",
+                    "hmax": "2982",
+                    "hsta": "OFF",
+                    "ffoc": "OFF",
+                    "tilt": "OK"
+                },
+                "scheduler": {
+                    "srsc": "e854",
+                    "dstv": "0000",
+                    "tzid": "0001"
+                }
+            }"#;
+
+        let expected = CurrentStateRaw{
+            msg: String::from("CURRENT-STATE"),
+            time: String::from("2020-05-19T14:53:04.000Z"),
+            mode_reason: String::from("LSCH"),
+            state_reason: String::from("ENV"),
+            dial: String::from("OFF"),
+            rssi: -35,
+            product_state: ProductState{
+                fan_mode: FanMode::Auto,
+                fan_state: FanState::On,
+                fan_speed: FanSpeed::Auto,
+                quality_target: QualityTarget::Better,
+                oscillation_status: OscillationStatus::On,
+                air_quality_monitoring_status: AirQualityMonitoringStatus::On,
+                filter_life: 3732,
+                ercd: String::from("NONE"),
+                night_mode: NightMode::On,
+                wacd: String::from("NONE"),
+                heat_mode: HeatMode::Off,
+                heat_target_kelvin: 298.2,
+                heat_state: HeatState::Off,
+                fan_focus_mode: FanFocusMode::Wide,
+                tilt_state: TiltState::No,
+            },
+            scheduler: SchedulerRaw{
+                srsc: String::from("e854"),
+                dstv: String::from("0000"),
+                tzid: String::from("0001")
+            }
+        };
+
+        let actual: CurrentStateRaw = serde_json::from_str(&raw).unwrap();
 
         assert_eq!(actual, expected);
     }
